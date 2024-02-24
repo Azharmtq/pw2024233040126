a832163f2381e9375a87b565001c71b7c868a6d0
Azharmtq <azhar.233040126@mail.unpas.ac.id>
2024-02-24 08:13:35 +0700

    Tugas 2a done

---
+++ Tugas/Tugas2/2a.php
@@ -0,0 +1,22 @@
+<?php
+$nama1 = "Azhar";
+$nama2 = "Muttaqien";
+$i = 1 ;
+while ($i<= 100)
+{
+    if ($i % 5 === 0 and $i % 3 === 0) {
+        echo $nama1." ".$nama2."<br>";
+    }
+    elseif ($i % 3 === 0) {
+            echo $nama1."<br>";
+        }
+        elseif ($i % 5 === 0) {
+            echo $nama2."<br>";
+        }
+        elseif (!$i % 5 === 0 or !$i % 3 === 0) {
+        echo $i."<br>";
+        }
+    
+    $i++;
+}
+?>
