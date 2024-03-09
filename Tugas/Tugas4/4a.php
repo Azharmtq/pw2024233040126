6c5e72e667b61df4fd35115cb42fd3526bd39499
Azharmtq <azhar.233040126@mail.unpas.ac.id>
2024-03-09 11:05:46 +0700

    tugas 4b done

--- Tugas/Tugas4/4a.php
+++ Tugas/Tugas4/4a.php
@@ -11,8 +11,6 @@ $kata = array("epan","cipta","akan","u","asa","esok");
 <body>
     <?php
     echo "M".($kata[4])." "."d".($kata[0])."mu"." "."di".($kata[1])."kan"." "."oleh"." "."apa"." "."yang"." "."kau"." "."kerj".($kata[2])." "."hari"." "."ini"." "."b".($kata[3])."kan"." "."b".($kata[5]);
-
-
     ?>
 </body>
 </html>
